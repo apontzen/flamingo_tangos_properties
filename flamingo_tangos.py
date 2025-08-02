1228d037bd28b6beedf0600ebf7b5a5ff686228a
Andrew Pontzen <andrew.p.pontzen@durham.ac.uk>
2025-08-02 09:23:52 -0400

    New properties for calculation

--- flamingo_tangos.py
+++ flamingo_tangos.py
@@ -22,7 +22,6 @@ class FlamingoInputHandler(tangos.input_handlers.pynbody.Gadget4HDFSubfindInputH
             f.wrap()
         return f
 
-
 class M200m(LiveHaloProperties):
     names = "M200m"
 
@@ -41,33 +40,73 @@ class M200m(LiveHaloProperties):
 
 class FlamingoDensityProfileBase(spherical_region.SphericalRegionPropertyCalculation):
     names = "_gas_density", "_gas_p", "_gas_entropy", \
-            "_gas_temp", "_gas_rho"
+            "_gas_temp", "_gas_rho", "_gas_vr", "_gas_vr_disp", "_gas_mass_enclosed", "_gas_mass_enclosed_2d", \
+            "_dm_mass_enclosed", "_dm_mass_enclosed_2d", "_dm_vr", "_dm_vr_disp", \
+            "_gas_mdot", "_gas_mdot_inflow", "_gas_mdot_outflow", \
+            "_dm_mdot", "_dm_mdot_inflow", "_dm_mdot_outflow"
+    
 
     _nbins = 50
     
     @centred_calculation
-    def calculate(self, data, existing_properties):
+    def calculate(self, data: pynbody.snapshot.SimSnap, existing_properties):
         minrad, maxrad  = self._get_min_max_radius(existing_properties)
 
-        data.gas['vol'] = data.gas['smooth']**3  # Volume for weighting
-
-        pro = pynbody.analysis.profile.Profile(data.gas, type='log', ndim=3,
-                                               min=minrad, max=maxrad, nbins=self._nbins,
-                                               weight_by='vol')
         
-        # pre-load data (unclear why this is necessary, but it is)
-        data.gas['Entropies']
-        data.gas['temp']
-        data.gas['rho']
-        data.gas['p']
 
-        den = pro['density']
-        p = pro['p']
-        entropy = pro['Entropies']
-        temp = pro['temp']
-        rho = pro['rho']
+        central_region = data[pynbody.filt.Sphere('25 kpc')]
+        vel_centre = np.average(central_region['vel'], axis=0, weights=central_region['mass'])
 
-        return den, p, entropy, temp, rho
+        try:
+            data.gas['vel']-=vel_centre
+            data.gas['vol'] = data.gas['smooth']**3  # Volume for weighting
+
+            pro = pynbody.analysis.profile.Profile(data.gas, type='log', ndim=3,
+                                                min=minrad, max=maxrad, nbins=self._nbins,
+                                                weight_by='vol')
+            
+            # pre-load data (unclear why this is necessary, but it is)
+            data.gas['Entropies']
+            data.gas['temp']
+            data.gas['rho']
+            data.gas['p']
+
+            den = pro['density']
+            p = pro['p']
+            entropy = pro['Entropies']
+            temp = pro['temp']
+            rho = pro['rho']
+
+            pro = pynbody.analysis.profile.Profile(data.gas, type='log', ndim=3,
+                                                min=minrad, max=maxrad, nbins=self._nbins)
+            vr = pro['vr']
+            vr_disp = pro['vr_disp']
+            mass_enc = pro['mass_enc']
+            mdot = pro['mdot']
+            mdot_inflow = pro['mdot_inflow']
+            mdot_outflow = pro['mdot_outflow']
+
+            pro = pynbody.analysis.profile.Profile(data.gas, type='log', ndim=2,
+                                                min=minrad, max=maxrad, nbins=self._nbins)
+            mass_enc_2d = pro['mass_enc']
+
+            pro = pynbody.analysis.profile.Profile(data.dm, type='log', ndim=3,
+                                                min=minrad, max=maxrad, nbins=self._nbins)
+            mass_enc_dm = pro['mass_enc']
+            vr_dm = pro['vr']
+            vr_disp_dm = pro['vr_disp']
+            mdot_dm = pro['mdot']
+            mdot_inflow_dm = pro['mdot_inflow']
+            mdot_outflow_dm = pro['mdot_outflow']
+
+            pro = pynbody.analysis.profile.Profile(data.dm, type='log', ndim=2,
+                                                min=minrad, max=maxrad, nbins=self._nbins)
+            mass_enc_2d_dm = pro['mass_enc']
+        finally:
+            data.gas['vel'] += vel_centre  
+
+        return den, p, entropy, temp, rho, vr, vr_disp, mass_enc, mass_enc_2d, mass_enc_dm, mass_enc_2d_dm, vr_dm, vr_disp_dm, \
+                mdot, mdot_inflow, mdot_outflow, mdot_dm, mdot_inflow_dm, mdot_outflow_dm
 
     def _get_min_max_radius(self, existing_properties):
         raise NotImplementedError("Subclasses must implement _get_min_max_radius method")
@@ -84,8 +123,11 @@ class FlamingoDensityProfileBase(spherical_region.SphericalRegionPropertyCalcula
     def plot_ylabel(self):
         return r"$\rho/M_{\odot}\,kpc^{-3}$", r"pressure/$M_{\odot} km^2 s^{-2} kpc^{-3}$", \
                r"entropy/$M_{\odot}^{-2/3} kpc^2 km^2 s^{-2}$", r"temperature/$K$",  \
-               r"$\rho/M_{\odot}\,kpc^{-3}$"
-    
+               r"$\rho/M_{\odot}\,kpc^{-3}$", r"velocity/$km/s$", r"vel dispersion/$km/s$", r"$M_{gas}/M_{\odot}$", r"$M_{gas,2D}/M_{\odot}$", \
+               r"$M_{dm}/M_{\odot}$", r"$M_{dm,2D}/M_{\odot}$", r"DM velocity/$km/s$", r"DM vel dispersion/$km/s$", \
+                r"$\dot{M}_{gas}/M_{\odot} yr^{-1}$", r"$\dot{M}_{gas,inflow}/M_{\odot} yr^{-1}$", r"$\dot{M}_{gas,outflow}/M_{\odot} yr^{-1}$", \
+                r"$\dot{M}_{dm}/M_{\odot} yr^{-1}$", r"$\dot{M}_{dm,inflow}/M_{\odot} yr^{-1}$", r"$\dot{M}_{dm,outflow}/M_{\odot} yr^{-1}$"
+
     def plot_xlog(self):
         return False
     
@@ -145,3 +187,71 @@ class FlamingoExclusiveDensityProfileAbsolute(FlamingoDensityProfileAbsolute):
     def calculate(self, data, existing_properties):
         data_exclusive = _filter_out_other_halos(data, existing_properties)
         return super().calculate(data_exclusive, existing_properties)
+    
+class FlamingoPrimordialBaryonicMassDeficit(spherical_region.SphericalRegionHaloProperties):
+    names = "primordial_baryonic_massfrac_deficit"
+
+    @centred_calculation
+    def calculate(self, data, existing_properties):
+        reference_mass = 5.65006349e+09 # Reference mass in Msun
+        mean_dm_mass = data.dm['mass'].mean()
+
+        # now work out the simulation particle mass, which is 2^n times reference_mass where n
+        # is an integer (but may be negative/zero/positive)
+        n = np.round(np.log2(mean_dm_mass / reference_mass), decimals=0)
+        sim_particle_mass = reference_mass * (2**n)
+
+        sim_gas_particle_mass = sim_particle_mass * 0.19565705 # OmB / OmC
+
+        # Calculate the primordial baryonic mass deficit
+        primordial_baryonic_massfrac_deficit = (mean_dm_mass - sim_particle_mass) / sim_gas_particle_mass
+
+        return primordial_baryonic_massfrac_deficit
+    
+    def requires_property(self):
+        return ["shrink_center", 'r200m'] + super().requires_property()
+    
+    def region_specification(self, db_data):
+        return pynbody.filt.Sphere(db_data['r200m'], db_data['shrink_center'])
+    
+
+
+
+@pynbody.analysis.profile.Profile.profile_property
+def mdot(profile: pynbody.analysis.profile.Profile):
+    # mdot = integral rho v_r r^2 d omega
+    # estimate in a spherical shell of thickness delta r:
+    # mdot = integral_r0^(r0+delta r) dr r^2 d omega (rho v_r) / delta r
+    #      = integral dV (rho v_r) / delta r
+    #      = sum m v_r / delta r
+    ar = profile['mv_r'] / np.diff(profile['bin_edges'])
+    ar.units = profile['mv_r'].units / profile['bin_edges'].units
+    return ar.in_units('Msol yr^-1')
+
+@pynbody.analysis.profile.Profile.profile_property
+def mdot_inflow(profile: pynbody.analysis.profile.Profile):
+    ar = -profile['mv_r_inflow'] / np.diff(profile['bin_edges'])
+    ar.units = profile['mv_r_inflow'].units / profile['bin_edges'].units
+    return ar.in_units('Msol yr^-1')
+
+@pynbody.analysis.profile.Profile.profile_property
+def mdot_outflow(profile: pynbody.analysis.profile.Profile):
+    ar = profile['mv_r_outflow'] / np.diff(profile['bin_edges'])
+    ar.units = profile['mv_r_outflow'].units / profile['bin_edges'].units
+    return ar.in_units('Msol yr^-1')
+
+@pynbody.derived_array
+def mv_r(f):
+    return f['vr'] * f['mass']
+
+@pynbody.derived_array
+def mv_r_inflow(f):
+    ar = f['mv_r'].copy()
+    ar[ar>0] = 0
+    return ar
+
+@pynbody.derived_array
+def mv_r_outflow(f):
+    ar = f['mv_r'].copy()
+    ar[ar<0] = 0
+    return ar
