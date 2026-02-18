6f6e28982f5f1751ce783f425db8d578b91357d5
Andrew Pontzen <andrew.p.pontzen@durham.ac.uk>
2026-02-18 12:12:48 +0000

    Fix issue with virial scalings calc

--- virial_scalings.py
+++ virial_scalings.py
@@ -8,9 +8,9 @@ def entropy(M200m, h=0.68, z=0, OmegaM0=0.307, OmegaB0=0.048):
     H0 = pynbody.units.Unit("100 km s^-1 Mpc^-1") * h
 
 
-    entrop = ((G*M200m*Msol)**(2,3) / 2) * ((1/400) * OmegaM0 * (8*np.pi*G)**2 / (9 * (H0)**2 * (1+z)**3 * OmegaB0**2))**(1,3)    
+    entrop = ((G*Msol)**(2,3) / 2) * ((1/400) * OmegaM0 * (8*np.pi*G)**2 / (9 * (H0)**2 * (1+z)**3 * OmegaB0**2))**(1,3)    
 
-    return entrop.in_units("Msol^-2/3 kpc^2 km^2 s^-2")
+    return entrop.in_units("Msol^-2/3 kpc^2 km^2 s^-2") * M200m**0.66666
 
 def velocity(M200m, h=0.68, z=0, OmegaM0=0.307):
     G = pynbody.units.Unit("G")
